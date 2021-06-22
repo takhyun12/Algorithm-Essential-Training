0774508f6078cb6709c63a4cc75366e064f6ca60
TACKHYUN JUNG <41291493+takhyun12@users.noreply.github.com>
2021-06-22 17:10:53 +0900

    Update test.py

--- Review Trainee Code/jyoonab/test.py
+++ Review Trainee Code/jyoonab/test.py
@@ -1,15 +1,30 @@
-def solution(index: int, text: str) -> str:
+'''
+문제: https://github.com/takhyun12/Algorithm-Essential-Training/blob/main/Practice/resistance.md
+'''
+def solution(resistance_dict: dict, color_list: list) -> int:
     # You can put your code in here
-    return [str]
-
+    return [int]
 
 if __name__ == '__main__':
-    cases = ["4, MISSPELL",
-             "1, PROGRAMMING",
-             "7, CONTEST",
-             "3, BALLOON"]
+    resistance_dict = {"black": [0, 1],
+                       "brown": [1, 10],
+                       "red": [2, 100],
+                       "orange": [3, 1000],
+                       "yellow": [4, 10000],
+                       "green": [5, 100000],
+                       "blue": [6, 1000000],
+                       "violet": [7, 10000000],
+                       "grey": [8, 100000000],
+                       "white": [9, 1000000000]}
 
-    for case in cases:
-        idx, txt = case.split(',')
-        result: str = solution(int(inx), txt.lstrip())
-        print(result)
+    color_list = ["yellow", "violet", "red"]
+    resistance_score = solution(resistance_dict, color_list)
+    print(resistance_score)  # 4700
+
+    color_list2 = ["white", "orange", "grey"]
+    resistance_score = solution(resistance_dict, color_list2)
+    print(resistance_score)  # 9300000000
+
+    color_list3 = ["green", "white", "brown"]
+    resistance_score = solution(resistance_dict, color_list3)
+    print(resistance_score)  # 590
