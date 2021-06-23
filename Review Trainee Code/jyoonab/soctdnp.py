8527bfed8ef732d7b207e4e5db0b492c00f2826e
jyoonab <jyoona12345@gmail.com>
2021-06-23 15:48:37 +0900

    Practice 1 Finished - version2

--- Review Trainee Code/jyoonab/soctdnp.py
+++ Review Trainee Code/jyoonab/soctdnp.py
@@ -1,25 +1,17 @@
 def solution(s: str) -> int:
-    # You can put your code in here
+    alphabet_str: str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    result: int = 0
 
-    MAX_NUM = 2015
-    add_num = 0
+    for i in alphabet_str:
+        if not i in s:
+            result += ord(i)
 
-    #if len(s) < 1 or len(s) > 1000:
-        #return -1
-
-    simplified_list = list(dict.fromkeys(sorted(s)))
-
-    for i in simplified_list:
-        #if (ord(i) > 90 or ord(i) < 65):
-        #    return -1
-        add_num += ord(i)
-
-    return MAX_NUM - add_num
+    return result
 
 
 if __name__ == '__main__':
     cases = ["ABCDEFGHIJKLMNOPQRSTUVW", "A", "BDCW", "K", "", 554342389243782437823478243872348234,
-    "CCBBAA", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+    "CCBBAA", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefg"]
     for case in cases:
         result: int = solution(str(case))
         print(result)
