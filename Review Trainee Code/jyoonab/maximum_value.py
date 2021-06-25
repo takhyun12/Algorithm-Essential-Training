e41c80f45a500440fca72f6841150dc73065b88c
jyoonab <jyoona12345@gmail.com>
2021-06-25 20:25:22 +0900

    max num practice finished

---
+++ Review Trainee Code/jyoonab/maximum_value.py
@@ -0,0 +1,14 @@
+def solution(sample_list: list) -> dict:
+    result: dict = {}
+    max_num: int = max(sample_list)
+    position_num: int = sample_list.index(max_num)
+    result["maximum value"] = max_num
+    result["index"] = position_num+1
+
+    # input your code
+    return result
+
+if __name__ == '__main__':
+    sample_list = [3, 29, 38, 12, 57, 74, 40, 85, 61]
+    print(solution(sample_list))
+    # input your code
