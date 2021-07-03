bd192ba6e971b16910ddc7a5a3f8ab1fb2d8a21e
TACKHYUN JUNG <41291493+takhyun12@users.noreply.github.com>
2021-07-03 16:12:41 +0900

    Update h_index.py

--- Solutions/h_index.py
+++ Solutions/h_index.py
@@ -1,6 +1,6 @@
 def solution(citation_list: list) -> int:
-    citation_list.sort(reverse=True)
-    return max(map(min, enumerate(citation_list, start=1)))
+    citation_list.sort(reverse=True) 
+    return max(map(min, enumerate(citation_list, start=1)))  # start=0 시, 가장 큰수가 min 연산에 미포함 됨 
 
 if __name__ == '__main__':
     citations: list = [3, 0, 6, 1, 5]
